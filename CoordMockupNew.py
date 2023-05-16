fe5735abe96fa07fbe05d1c9cb36f9d0b0e2da2b
Fabian Hummer <fabian.hummer@kit.edu>
2023-05-16 16:52:20 +0200

    Add documentation to geometry scripts

--- CoordMockupNew.py
+++ CoordMockupNew.py
@@ -1,13 +1,22 @@
+""" CoordMockupNew.py
+
+This file demonstrates the different functions of the classes TileBoard and
+TileBoardCoordinates.
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 from TileBoardCoordinates import *
 
 
+# Define the coordinate system and the tileboards
+# The origin is chosen such that it matches with the D8 floorplan
 coord = TileBoardCoordinates(base_origin=np.array([0, -1791.1]), base_rotation=90)
 board = TileBoard(coord)
 
-# Track mouse position
+
+# Track mouse position and show the user which channel they are pointing at
 def mouse_move(event):
     x, y = event.xdata, event.ydata
     if x is not None and y is not None:
@@ -15,16 +24,16 @@ def mouse_move(event):
         plt.title("r, phi = {:.2f}, {:.2f}\n Channel {}".format(r, phi, board.GetChannelByPosition(x, y)))
         plt.draw()
 
+
 plt.connect('motion_notify_event', mouse_move)
 
 
-# Get drawing axes
+# Visualize the board
 ax = plt.gca()
 ax.set_aspect("equal")
 
 board.DrawOutline(ax)
 board.DrawSiPMs(ax, label_pos=False)
 board.DrawTiles(ax)
-
 
 plt.show()
--- GenerateTargets.py
+++ GenerateTargets.py
@@ -1,33 +1,40 @@
+""" GenerateTargets.py
+
+A script used during the tileboard test beam at DESY on 2023-05-16.
+This script generates a CSV file with beam positions that can be used for the
+automatic stage to scan every tile on the tileboard.
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
 from TileBoardCoordinates import *
 
+# This origin we determined by finding where the beam hits the board.
+# It should be different every time you move the setup.
 base_origin = np.array([-325 - 35.8, -665 - 1641.2])
 
+# Define the used utility classes
 coord = TileBoardCoordinates(base_origin=base_origin, base_rotation=90)
 board = TileBoard(coord)
 
-
-# Generate reasonable hit positions
+# Generate reasonable hit positions in the polar coordinate system
 angles = board.SiPM_angles + 0.25
 radii = board.SiPM_radii + 7
 
 all_angles = np.tile(angles, 8)
 all_radii = np.repeat(radii, 8)
 
-print(all_angles)
-print(all_radii)
-
+# Convert the positions to the cartesian system
 x_hits, y_hits = coord.PolarToCartesian(all_radii, all_angles)
 x_hits = -x_hits
 
+# Write the hit positions to a CSV file
 df = pd.DataFrame(data={"ch": board.SiPM_channels, "x": x_hits, "y": y_hits})
 df.to_csv("hit_positions.csv")
 
-
-# Get drawing axes
+# Draw the positions for debugging
 ax = plt.gca()
 ax.set_aspect("equal")
 
@@ -36,6 +43,5 @@ board.DrawSiPMs(ax, label_pos=False)
 board.DrawTiles(ax)
 
 board.DrawSiPMs(ax, radii=radii, angles=angles, format_str="rx")
-
 
 plt.show()
